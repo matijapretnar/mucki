7a07638f2bd95c074e72773a7b3f47587cff0059
Matija Pretnar <matija@pretnar.info>
2022-06-06 11:18:11 +0200

    Približna simulacija obstoječega modela
    https://www.cathouseonthekings.com/docs/populationgrowth.php

--- src/model.ml
+++ src/model.ml
@@ -1,7 +1,91 @@
-type model = unit
+type generation = {
+  age : int;
+  fertile_females : int;
+  infertile_females : int;
+  males : int;
+}
 
-let init : model = ()
+type percentage = Percent of int
 
-type msg = unit
+let take_percentage n (Percent p) = n * p / 100
 
-let update (_model : model) : msg -> model = function () -> ()
+type model = {
+  litters_per_year : int;
+  kittens_per_litter : int;
+  percentage_of_female_kittens : percentage;
+  percentage_of_kittens_who_survive_to_sexual_maturity : percentage;
+  percentage_of_fertile_sexually_mature_females : percentage;
+  average_lifespan_of_a_feral_cat : int;
+  years_to_project : int;
+  population : generation list;
+}
+
+let init : model =
+  {
+    litters_per_year = 2;
+    kittens_per_litter = 4;
+    percentage_of_female_kittens = Percent 50;
+    percentage_of_kittens_who_survive_to_sexual_maturity = Percent 70;
+    percentage_of_fertile_sexually_mature_females = Percent 100;
+    average_lifespan_of_a_feral_cat = 4;
+    years_to_project = 7;
+    population =
+      [ { age = 1; fertile_females = 1; infertile_females = 0; males = 1 } ];
+  }
+
+let active_females model =
+  model.population
+  |> List.map (fun generation -> generation.fertile_females)
+  |> List.fold_left ( + ) 0
+
+let generation_size generation =
+  generation.fertile_females + generation.infertile_females + generation.males
+
+let population_size model =
+  model.population |> List.map generation_size |> List.fold_left ( + ) 0
+
+let newborn_generation model =
+  let kittens = active_females model * model.kittens_per_litter in
+  let females = take_percentage kittens model.percentage_of_female_kittens in
+  let males = kittens - females in
+  { age = 0; fertile_females = females; infertile_females = 0; males }
+
+let kittens_born model = generation_size (newborn_generation model)
+
+let grow_generation model generation =
+  match generation.age with
+  | 0 ->
+      let females =
+        take_percentage
+          (generation.infertile_females + generation.fertile_females)
+          model.percentage_of_kittens_who_survive_to_sexual_maturity
+      in
+      let fertile_females =
+        take_percentage females
+          model.percentage_of_fertile_sexually_mature_females
+      in
+      let infertile_females = females - fertile_females in
+      let males =
+        take_percentage generation.males
+          model.percentage_of_kittens_who_survive_to_sexual_maturity
+      in
+      Some { age = 1; fertile_females; infertile_females; males }
+  | age
+    when age < model.average_lifespan_of_a_feral_cat * model.litters_per_year ->
+      Some { generation with age = generation.age + 1 }
+  | _ -> None
+
+let grow_population model =
+  let newborns = newborn_generation model in
+  let adults = List.filter_map (grow_generation model) model.population in
+  { model with population = newborns :: adults }
+
+let rec history model = function
+  | 0 -> []
+  | n -> model :: history (grow_population model) (n - 1)
+
+type msg = GrowPopulation | SetModel of model
+
+let update (model : model) : msg -> model = function
+  | GrowPopulation -> grow_population model
+  | SetModel model -> model
--- src/view.ml
+++ src/view.ml
@@ -1,3 +1,104 @@
 open Vdom
 
-let view (_model : Model.model) = text "Mucki"
+let view_int n = text (Printf.sprintf "%d" n)
+let view_float x = text (Printf.sprintf "%f" x)
+let string_of_percentage (Model.Percent p) = Printf.sprintf "%d %%" p
+let view_percentage p = text (string_of_percentage p)
+let view_label lbl view = div [ text (Printf.sprintf "%s: " lbl); view ]
+
+let dropdown ?default options view (msg : 'a -> Model.msg) =
+  let view_option v =
+    if Some v = default then
+      elt "option" ~a:[ attr "selected" "true" ] [ text (view v) ]
+    else elt "option" [ text (view v) ]
+  in
+  div
+    ~a:[ class_ "select"; onchange_index (fun i -> msg (List.nth options i)) ]
+    [ elt "select" (List.map view_option options) ]
+
+let int_dropdown ?default from_int to_int msg =
+  let options = List.init (to_int - from_int + 1) (fun i -> from_int + i) in
+  dropdown ?default options string_of_int msg
+
+let percentage_dropdown ?default msg =
+  let options = List.init 101 (fun i -> Model.Percent i) in
+  dropdown ?default options string_of_percentage msg
+
+let float_dropdown ?default from_float to_float step msg =
+  let length = int_of_float ((to_float -. from_float) /. step) in
+  let options =
+    List.init length (fun i -> from_float +. (float_of_int i *. step))
+  in
+  dropdown ?default options string_of_float msg
+
+let view_parameters (model : Model.model) =
+  div
+    [
+      view_label "Litters per year"
+        (int_dropdown ~default:model.litters_per_year 1 4
+           (fun litters_per_year ->
+             Model.SetModel { model with litters_per_year }));
+      view_label "Kittens per litter"
+        (int_dropdown ~default:model.kittens_per_litter 1 8
+           (fun kittens_per_litter ->
+             Model.SetModel { model with kittens_per_litter }));
+      view_label "Percentage of female kittens"
+        (percentage_dropdown ~default:model.percentage_of_female_kittens
+           (fun percentage_of_female_kittens ->
+             Model.SetModel { model with percentage_of_female_kittens }));
+      view_label "Percentage of kittens who survive to sexual maturity"
+        (percentage_dropdown
+           ~default:model.percentage_of_kittens_who_survive_to_sexual_maturity
+           (fun percentage_of_kittens_who_survive_to_sexual_maturity ->
+             Model.SetModel
+               {
+                 model with
+                 percentage_of_kittens_who_survive_to_sexual_maturity;
+               }));
+      view_label "Percentage of fertile sexually mature females"
+        (percentage_dropdown
+           ~default:model.percentage_of_fertile_sexually_mature_females
+           (fun percentage_of_fertile_sexually_mature_females ->
+             Model.SetModel
+               { model with percentage_of_fertile_sexually_mature_females }));
+      view_label "Average lifespan of a feral cat"
+        (int_dropdown ~default:model.average_lifespan_of_a_feral_cat 1 20
+           (fun average_lifespan_of_a_feral_cat ->
+             Model.SetModel { model with average_lifespan_of_a_feral_cat }));
+      view_label "Years to project"
+        (int_dropdown ~default:model.years_to_project 1 20
+           (fun years_to_project ->
+             Model.SetModel { model with years_to_project }));
+    ]
+
+let view_generation (generation : Model.generation) =
+  div
+    [
+      view_label "Age" (view_int generation.age);
+      view_label "Fertile females" (view_int generation.fertile_females);
+      view_label "Infertile females" (view_int generation.infertile_females);
+      view_label "Males" (view_int generation.males);
+    ]
+
+let view_population (model : Model.model) =
+  div
+    [
+      view_label "Active females" (view_int (Model.active_females model));
+      view_label "Kittens born" (view_int (Model.kittens_born model));
+      view_label "Total population" (view_int (Model.population_size model));
+      elt "ul"
+        (List.map
+           (fun gen -> elt "li" [ view_generation gen ])
+           model.population);
+    ]
+
+let view (model : Model.model) =
+  div
+    [
+      input []
+        ~a:
+          [ onclick (fun _ -> Model.GrowPopulation); type_button; value "Grow" ];
+      view_parameters model;
+      elt "hr" [];
+      view_population model;
+    ]
